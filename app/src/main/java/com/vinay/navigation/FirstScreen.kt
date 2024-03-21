79a03ea7982de0bdc9b97e17f2b55df74c9b7969
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2024-03-21 03:45:41 -0400

    Screens setup

---
+++ app/src/main/java/com/vinay/navigation/FirstScreen.kt
@@ -0,0 +1,35 @@
+package com.vinay.navigation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FirstScreen() {
+    val name = remember {
+        mutableStateOf("")
+    }
+
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "First Screen")
+        OutlinedTextField(value = name.value, onValueChange = {
+            name.value = it
+        })
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Go To Second Screen")
+        }
+
+    }
+}
